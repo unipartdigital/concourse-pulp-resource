63d3137031dbca923ba3a88b579b1578ebc7c181
Ash Crosby <tcrosby@gmail.com>
2022-03-09 17:52:34 +0000

    fix check

--- scripts/out.d/deb.sh
+++ scripts/out.d/deb.sh
@@ -12,15 +12,15 @@ for deb in $deb_files; do
   initial=${package_name:0:1}
   deb_name=$(echo $deb | rev | cut -d"/" -f1  | rev)
   relative_path="pool/${initial}/${package_name}/${deb_name}"
-  file_count=$(http GET ${BASE_ADDR}/pulp/api/v3/content/deb/packages/?relative_path=${relative_path} Authorization:"$BASIC_AUTH" | jq -r '.count')
+  file_count=$(http GET ${ENDPOINT}/pulp/api/v3/content/deb/packages/?relative_path=${relative_path} Authorization:"$BASIC_AUTH" | jq -r '.count')
   echo "Count: $file_count"
   if [[ $file_count -eq "0" ]]; then
     echo "Uploading $deb"
-    export TASK_URL=$(http --form POST ${BASE_ADDR}/pulp/api/v3/content/deb/packages/ file@"$deb" Authorization:"$BASIC_AUTH" | jq -r '.task')
+    export TASK_URL=$(http --form POST ${ENDPOINT}/pulp/api/v3/content/deb/packages/ file@"$deb" Authorization:"$BASIC_AUTH" | jq -r '.task')
     sleep 2
-    export CONTENT_HREF=$(http GET ${BASE_ADDR}${TASK_URL} Authorization:"$BASIC_AUTH" | jq -r '.created_resources | first')
+    export CONTENT_HREF=$(http GET ${ENDPOINT}${TASK_URL} Authorization:"$BASIC_AUTH" | jq -r '.created_resources | first')
   else
-    CONTENT_HREF=$(http GET ${BASE_ADDR}/pulp/api/v3/content/deb/packages/?relative_path=${relative_path} Authorization:"$BASIC_AUTH" | jq -r '.results[0].pulp_href')
+    CONTENT_HREF=$(http GET ${ENDPOINT}/pulp/api/v3/content/deb/packages/?relative_path=${relative_path} Authorization:"$BASIC_AUTH" | jq -r '.results[0].pulp_href')
   fi
-  http POST ${BASE_ADDR}${REPO_HREF}modify/ add_content_units:="[\"${CONTENT_HREF}\"]" Authorization:"$BASIC_AUTH"
+  http POST ${ENDPOINT}${REPO_HREF}modify/ add_content_units:="[\"${CONTENT_HREF}\"]" Authorization:"$BASIC_AUTH"
 done
